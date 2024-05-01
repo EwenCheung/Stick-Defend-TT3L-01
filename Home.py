03df4afc56ffb23ffeb17de726ae76b437523eea
Ewen Cheung <yiwencheung@gmail.com>
2024-05-01 20:34:38 +0800

    fix bug and warning , and do the loading page choose game after 100 %

--- Home.py
+++ Home.py
@@ -3,10 +3,8 @@ from sys import exit
 
 pygame.init()
 
-class StartButton():
-    pass
 
-class LoadingBar():
+class LoadingBar:
     def __init__(self, x, y, height, width, colour, border_colour, border_width):
         self.x = x
         self.y = y
@@ -15,26 +13,34 @@ class LoadingBar():
         self.colour = colour
         self.border_colour = border_colour
         self.border_width = border_width
-        self.progress = 0 # the first progress variable is to calculate how much should it be filled in
-                          # if remove that the loading bar wont work
+        self.progress = 0  # the first progress variable is to calculate how much should it be filled in
+        # if remove that the loading bar won't work
 
     def draw_bar(self, screen):
-        pygame.draw.rect(screen, self.border_colour, (self.x - self.border_width, self.y - self.border_width, self.width + 2*self.border_width, self.height + 2*self.border_width))
+        pygame.draw.rect(screen, self.border_colour, (
+            self.x - self.border_width, self.y - self.border_width, self.width + 2 * self.border_width,
+            self.height + 2 * self.border_width))
         pygame.draw.rect(screen, self.colour, (self.x, self.y, self.width * self.progress, self.height))
 
-class Game():
+
+class Game:
     def __init__(self):
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1000,600))
+        self.screen = pygame.display.set_mode((1000, 600))
         pygame.display.set_caption('Home Page')
         self.loading_bar = LoadingBar(400, 500, 30, 200, (255, 255, 224), (0, 0, 0), 2)  # Fixed width initialization
-        self.progress = 0  # this one is if i put 0.2 it will start the loading bar from 0.2
+        self.progress = 0  # this one is if I put 0.2 it will start the loading bar from 0.2
 
-        self.set_up()
-
-    def set_up(self):
-        self.image = pygame.image.load("War of stick/Picture/utlis/background_photo.jpg")  # Replace "home_image.jpg" with your image path
+        self.image = pygame.image.load(
+            "War of stick/Picture/utils/background_photo.jpg")  # Replace "home_image.jpg" with your image path
         self.image_rect = self.image.get_rect(center=(1000 // 2, 600 // 2))
+
+        self.wood_plank_surface = pygame.image.load('Plant vs Stick/Picture/utils/wood.png').convert()
+        self.wood_plank_surface = pygame.transform.scale(self.wood_plank_surface, (140, 50))
+        self.wood_plank_rectangle = self.wood_plank_surface.get_rect(topleft=(850, 10))
+
+        self.loading = True
+        self.finish_loading = False
 
     def event_handling(self):
         for event in pygame.event.get():
@@ -44,15 +50,34 @@ class Game():
 
     def game_start(self):
         self.screen.blit(self.image, self.image_rect)
-        self.loading_bar.draw_bar(self.screen)
 
     def update_progress(self):
         # Simulating loading progress
-        self.progress += 0.005
-        if self.progress >= 1:
+
+        if self.progress <= 1:
+            self.progress += 0.005
+            self.finish_loading = False
+
+        else:
             self.progress = 1
+            self.finish_loading = True
+            self.loading = False
 
         self.loading_bar.progress = self.progress
+        self.loading_bar.draw_bar(self.screen)
+
+    def choose_game(self):
+        self.wood_plank_rectangle = self.wood_plank_surface.get_rect(center=(350, 430))
+        self.screen.blit(self.wood_plank_surface, self.wood_plank_rectangle)
+        pvz = pygame.font.Font(None, 40).render('Plant vs Zombie', True, (255, 255, 255))
+        pvz_rect = pvz.get_rect(center=(350, 430))
+        self.screen.blit(pvz, pvz_rect)
+
+        self.wood_plank_rectangle = self.wood_plank_surface.get_rect(center=(650, 430))
+        self.screen.blit(self.wood_plank_surface, self.wood_plank_rectangle)
+        stick_of_war = pygame.font.Font(None, 40).render("Stick of War", True, (255, 255, 255))
+        stick_of_war_rect = stick_of_war.get_rect(center=(650, 430))
+        self.screen.blit(stick_of_war, stick_of_war_rect)
 
     def run(self):
         while True:
@@ -60,12 +85,14 @@ class Game():
 
             self.event_handling()
             self.game_start()
-            self.update_progress()
+            if self.loading:
+                self.update_progress()
+            elif self.finish_loading:
+                self.choose_game()
 
             pygame.display.update()
             self.clock.tick(60)
 
+
 if __name__ == '__main__':
     Game().run()
-
-
