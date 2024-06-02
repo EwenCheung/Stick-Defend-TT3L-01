48dfcd024f22848de35d72e304b0cff12a6ea635
Ewen Cheung <yiwencheung@gmail.com>
2024-06-02 13:33:09 +0800

    push

--- Home.py
+++ Home.py
@@ -40,48 +40,70 @@ class GameHome:
         self.image_rect = self.image.get_rect(center=(1000 // 2, 600 // 2))
 
         self.wood_plank_surface = pygame.image.load('Plant vs Stick/Picture/utils/wood.png').convert()
-        self.wood_plank_surface = pygame.transform.scale(self.wood_plank_surface, (200, 80))
+        self.wood_plank_surface = pygame.transform.scale(self.wood_plank_surface, (200, 60))
         self.pokemon_vs_naruto_rect = None
         self.stick_of_war_rect = None
 
         self.text_box_surface = pygame.image.load('Plant vs Stick/Picture/utils/wood.png').convert()
-        self.text_box_surface = pygame.transform.scale(self.text_box_surface, (400, 80))
+        self.text_box_surface = pygame.transform.scale(self.text_box_surface, (400, 60))
 
         self.loading = True
         self.finish_loading = False
 
         self.font = pygame.font.Font(None, 40)
         self.first_time = True
-        self.choose_to_play = False
-        self.choosing_method = True
+        self.choose_game_to_play = False
+        self.choosing_login_method = True
         self.signing_in = False
         self.signing_up = False
-        self.guest = False
+        self.login_as_guest = False
         self.key_user = False
         self.key_pass = False
+
+
+        #sign up
+        self.user_text_box_rectangle = self.text_box_surface.get_rect(center=(500, 250))
+        self.ask_username = self.font.render('Choose your username', True, (255, 255, 255))
+        self.ask_username_rect = self.ask_username.get_rect(center=(500, 200))
+
+        self.pass_text_box_rectangle = self.text_box_surface.get_rect(center=(500, 350))
+        self.ask_password = self.font.render("Enter password", True, (255, 255, 255))
+        self.ask_password_rect = self.ask_password.get_rect(center=(500, 300))
+
+        self.enter_rectangle = self.wood_plank_surface.get_rect(center=(500, 450))
+        self.enter_text = self.font.render("Enter", True, (255, 255, 255))
+        self.enter_text_rect = self.enter_text.get_rect(center=self.enter_rectangle.center)
+
+        self.back_rectangle = self.wood_plank_surface.get_rect(center=(100, 100))
+        self.back_text = self.font.render("Back", True, (255, 255, 255))
+        self.back_text_rect = self.back_text.get_rect(center=self.back_rectangle.center)
+
         self.sign_up_username = ""
         self.sign_up_password = ""
+        self.signup_done = False
+
+
 
     def event_handling(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            elif self.choose_game_to_play and event.type == pygame.MOUSEBUTTONDOWN:
                 if self.stick_of_war_rect.collidepoint(pygame.mouse.get_pos()):
                     self.go_level_py()
                 elif self.pokemon_vs_naruto_rect.collidepoint(pygame.mouse.get_pos()):
                     self.go_pokemon_py()
-            if self.choosing_method and event.type == pygame.MOUSEBUTTONDOWN:
-                if self.sign_in_rect.collidepoint(pygame.mouse.get_pos()):
+            if self.choosing_login_method and event.type == pygame.MOUSEBUTTONDOWN:
+                if self.sign_in_rect.collidepoint(pygame.mouse.get_pos()) and not self.signing_up and not self.login_as_guest:
                     self.signing_in = True
-                    self.choose_to_play = False
-                elif self.sign_up_rect.collidepoint(pygame.mouse.get_pos()):
+                    self.choosing_login_method = False
+                elif self.sign_up_rect.collidepoint(pygame.mouse.get_pos()) and not self.signing_in and not self.login_as_guest:
                     self.signing_up = True
-                    self.choose_to_play = False
-                elif self.guest_rect.collidepoint(pygame.mouse.get_pos()):
-                    self.guest = True
-                    self.choose_to_play = False
+                    self.choosing_login_method = False
+                elif self.login_as_guest_rect.collidepoint(pygame.mouse.get_pos()) and not self.signing_in and not self.signing_up:
+                    self.login_as_guest = True
+                    self.choosing_login_method = False
 
             if self.signing_up:
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -91,6 +113,17 @@ class GameHome:
                     elif self.pass_text_box_rectangle.collidepoint(pygame.mouse.get_pos()):
                         self.key_user = False
                         self.key_pass = True
+                    elif self.back_rectangle.collidepoint(pygame.mouse.get_pos()):
+                        self.key_user = False
+                        self.key_pass = False
+                        self.signing_up = False
+                        self.choosing_login_method = True
+                    elif self.enter_rectangle.collidepoint(pygame.mouse.get_pos()):
+                        self.key_user = False
+                        self.key_pass = False
+                        self.signing_up = False
+                        self.choosing_login_method = True
+                        self.signup_done = True
                 if event.type == pygame.KEYDOWN:
                     if self.key_user:
                         if event.key == pygame.K_BACKSPACE:
@@ -102,6 +135,7 @@ class GameHome:
                             self.sign_up_password = self.sign_up_password[:-1]
                         else:
                             self.sign_up_password += event.unicode
+
 
     def go_pokemon_py(self):
         pygame.quit()  # Cleanup before switching
@@ -166,29 +200,34 @@ class GameHome:
         wood_plank_rectangle = self.wood_plank_surface.get_rect(center=(500, 300))
         self.screen.blit(self.wood_plank_surface, wood_plank_rectangle)
         guest = self.font.render("Login as Guest", True, (255, 255, 255))
-        self.guest_rect = guest.get_rect(center=(500, 300))
-        self.screen.blit(guest, self.guest_rect)
+        self.login_as_guest_rect = guest.get_rect(center=(500, 300))
+        self.screen.blit(guest, self.login_as_guest_rect)
 
     def signing_in(self):
         pass
 
     def sign_up(self):
-        self.user_text_box_rectangle = self.text_box_surface.get_rect(center=(350, 250))
+        # Draw username elements
+        self.screen.blit(self.ask_username, self.ask_username_rect)
         self.screen.blit(self.text_box_surface, self.user_text_box_rectangle)
-        ask_username = self.font.render('Choose your username', True, (255, 255, 255))
-        self.sign_up_rect = ask_username.get_rect(center=(350, 200))
-        self.screen.blit(ask_username, self.sign_up_rect)
+        self.username = self.font.render(self.sign_up_username, True, (255, 255, 255))
+        username_rect = self.username.get_rect(center=self.user_text_box_rectangle.center)
+        self.screen.blit(self.username, username_rect.move(0, 0))
 
-        self.pass_text_box_rectangle = self.text_box_surface.get_rect(center=(650, 430))
-        self.screen.blit(self.text_box_surface, self.user_text_box_rectangle)
-        ask_password = self.font.render("Enter password", True, (255, 255, 255))
-        self.sign_in_rect = ask_password.get_rect(center=(650, 450))
-        self.screen.blit(ask_password, self.sign_in_rect)
+        # Draw password elements
+        self.screen.blit(self.ask_password, self.ask_password_rect)
+        self.screen.blit(self.text_box_surface, self.pass_text_box_rectangle)
+        self.password = self.font.render(self.sign_up_password, True, (255, 255, 255))
+        password_rect = self.password.get_rect(center=self.pass_text_box_rectangle.center)
+        self.screen.blit(self.password, password_rect.move(0, 0))
 
-        username = self.font.render(self.sign_up_username, True, (255, 255, 255))
-        self.screen.blit(username, (350, 250))
-        password = self.font.render(self.sign_up_password, True, (255, 255, 255))
-        self.screen.blit(password, (650, 430))
+        # Draw enter button
+        self.screen.blit(self.wood_plank_surface, self.enter_rectangle)
+        self.screen.blit(self.enter_text, self.enter_text_rect)
+
+        # Draw back button
+        self.screen.blit(self.wood_plank_surface, self.back_rectangle)
+        self.screen.blit(self.back_text, self.back_text_rect)
 
     def run(self):
         while True:
@@ -200,13 +239,13 @@ class GameHome:
                 if self.loading:
                     self.update_progress()
                 elif self.finish_loading:
-                    if self.choosing_method:
+                    if self.choosing_login_method:
                         self.signing_user()
                     elif self.signing_in:
                         pass
                     elif self.signing_up:
                         self.sign_up()
-                    else:
+                    elif self.choose_game_to_play:
                         self.choose_game()
             else:
                 self.choose_game()
